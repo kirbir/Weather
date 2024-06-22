666b6ffd3c13b9d480d8ab66574a1dda26259a03
BBking <birkirvr@gmail.com>
2024-06-22 12:39:25 +0000

    Commit everything

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,13 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+
+  Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.mainCondition,
+    });
+
+    factory Weather.fromJson
+}
